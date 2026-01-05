a8fb055f35bcdd11e0a3e0b011acc5a0ee57b797
bishika <bishikachaudhary0707@gmail.com>
2026-01-05 12:02:57 +0545

    backend completed and front end started

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { singleUpload } from "../middlewares/multer.js";
 
 
 
 const router = express.Router();
-router.post("/register", register);
-// router.route("/register").post(register);
+router.post("/register", singleUpload, register);
+router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated, updateProfile);
---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import {
+  Avatar,
+  AvatarImage,
+  AvatarFallback,
+} from "@/components/ui/avatar";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { LogOut, User2 } from "lucide-react";
+import { Link } from "react-router-dom";
+import Login from "../auth/login";
+
+const Navbar = () => {
+  const user = false;
+  return (
+    <nav className="bg-white shadow-sm">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16 px-4">
+        <h1 className="text-2xl font-bold">
+          job<span className="text-[#F83002]">portal</span>
+        </h1>
+
+        <div className="flex items-center gap-12">
+
+          <ul className="flex font-medium items-center gap-5">
+            <li className="cursor-pointer hover:text-primary transition">Home</li>
+            <li className="cursor-pointer hover:text-primary transition">Jobs</li>
+            <li className="cursor-pointer hover:text-primary transition">Browse</li>
+          </ul>
+          {
+            !user ? (
+              <div className="flex items-center gap-2">
+                <Link to="/Login"><Button variant="outline">Login</Button></Link>
+                <Link to="/Signup"><Button className="bg-[#6A38C2] hover:bg-[#3f138d]">Signup</Button>
+                </Link>
+
+              </div>
+            ) : (
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>BC</AvatarFallback>
+                  </Avatar>
+                </PopoverTrigger>
+
+                <PopoverContent className="w-80 p-4 space-y-4">
+                  <div className="flex items-center gap-4">
+                    <Avatar>
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                      <AvatarFallback>BC</AvatarFallback>
+                    </Avatar>
+                    <div>
+                      <h4 className="font-medium">Bishika Chaudhary</h4>
+                      <p className="text-sm text-muted-foreground">
+                        Full-stack Developer
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-col my-2 text-gray-600">
+                    <div className="flex w-fit items-center gap-2 cursor-pointer focus:outline-none">
+                      <User2 />
+                      <Button variant="link" className="w-full">
+                        view profile
+                      </Button>
+
+                    </div>
+
+                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                      <LogOut />
+                      <Button variant="link" className="w-full">
+                        Logout
+                      </Button>
+                    </div>
+                  </div>
+
+
+                </PopoverContent>
+              </Popover>
+            )
+          }
+
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
